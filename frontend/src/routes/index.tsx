6fd355702b041660ed18a6fa91a298e83378f36b
username123115 <mrpickles123115@gmail.com>
2025-05-11 15:20:02 -0600

    change tsconfig so tsserver can read path aliases

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 // Funny nvim plugin why do you error here
 // I'm actually going to crash out what in the skibidi rizz is this
+// It doesn't work in VSCode too chat is this real
+// Oh my gyatt I had to have the two referenced tsconfigs extend base
 import Entrance from '@components/entrance.tsx'
 
 export const Route = createFileRoute('/')({

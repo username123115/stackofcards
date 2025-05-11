76e71411da38fe49b8820e9a58ed8bbef821bcf6
username123115 <mrpickles123115@gmail.com>
2025-05-11 12:30:52 -0600

    style changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.module.css'
+import './styles/app.module.css'
+
+
 
 import App from './App.tsx'
-import Header from './components/utility/header.tsx'
-import Footer from './components/utility/footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<Header />
 		<App />
-		<Footer />
 	</StrictMode>,
 )
-

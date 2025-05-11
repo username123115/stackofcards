4f725338055d2c24921c43b7594bda72f7429806
username123115 <mrpickles123115@gmail.com>
2025-05-11 13:19:38 -0600

    tanstack quickstart

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import './styles/app.module.css'
 
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
 
+const router = createRouter({ routeTree })
 
-import App from './App.tsx'
+// TODO: I have no idea what this does 🥀
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 )

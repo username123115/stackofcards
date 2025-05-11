31c77c84060272837259a07b9230c2f2e86124af
username123115 <mrpickles123115@gmail.com>
2025-05-10 18:08:50 -0600

    add form validation

--- frontend/src/components/entrance.tsx
+++ frontend/src/components/entrance.tsx
@@ -1,24 +1,58 @@
+import { useState } from 'react';
 import styles from './components.module.css'
 
 function Entrance() {
+	const [codeInput, setCodeInput] = useState<string>('');
+	const message = Validate(codeInput);
 	return (
 		<>
 
 			<div className={styles.entrance}>
-				<div className={styles.join}>
+				<form className={styles.join}>
 					<h1> Join game </h1>
-					<input value="Enter Code" />
+					<input
+						value={codeInput}
+						type="text"
+						onChange={(e) => setCodeInput(e.target.value)}
+					/>
 					<button className={styles.button}> Join </button>
+
+					<div className={styles.info}>
+						{message}
+					</div>
+
+				</form>
+
+				<div className={styles.or}>
+					<h1>
+						OR
+					</h1>
 				</div>
 
 				<div className={styles.create}>
-					<button className={styles.button}> Create Game </button>
+					<button className={styles.gameButton}> Create Game </button>
 
 				</div>
 
 			</div>
 		</>
 	)
+}
+
+function Validate(n: string): string | null {
+	if (n.length === 0) {
+		return "Enter a code to join";
+	}
+	const allowed: string = "0123456789";
+	for (let i = 0; i < n.length; i++) {
+		if (!allowed.includes(n[i])) {
+			return "Enter only numeric values";
+		}
+	}
+	if (n.length !== 6) {
+		return "Please enter a 6 digit code";
+	}
+	return null;
 }
 
 export default Entrance

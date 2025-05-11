91a0e3fa860918ea06ffafa4d468d50de939f2bf
username123115 <mrpickles123115@gmail.com>
2025-05-11 14:55:36 -0600

    crashing out over neovim language server

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
+// Funny nvim plugin why do you error here
+// I'm actually going to crash out what in the skibidi rizz is this
+import Entrance from '@components/entrance.tsx'
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent,
+	component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+	return (
+		<>
+			<Entrance />
+		</>
+	)
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +13,11 @@ export default defineConfig({
 				changeOrigin: true,
 			}
 		}
-	}
+	},
+	resolve: {
+		alias: {
+			'@components': path.resolve(__dirname, './src/components'),
+		},
+
+	},
 })

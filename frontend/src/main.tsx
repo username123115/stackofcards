099b4c8e4c7de203636d5eae35a7ebff63530ad2
username123115 <mrpickles123115@gmail.com>
2025-05-11 18:50:16 -0600

    set up query client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import './styles/app.module.css'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+import './styles/app.module.css'
+
+const queryClient = new QueryClient()
 
 const router = createRouter({ routeTree })
 
-// TODO: I have no idea what this does 🥀
+// TODO: I have no idea what ts does 🥀
 declare module '@tanstack/react-router' {
 	interface Register {
 		router: typeof router
@@ -17,6 +22,9 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</StrictMode>,
 )
--- frontend/src/routes/test.tsx
+++ frontend/src/routes/test.tsx
@@ -1,9 +1,49 @@
 import { createFileRoute } from '@tanstack/react-router'
+import axios from 'axios';
+
+import { useQuery } from '@tanstack/react-query'
+
+//makes a request to backend 
+
+interface TestObj {
+	views: number;
+	name: string;
+}
+
+
 
 export const Route = createFileRoute('/test')({
-  component: RouteComponent,
+	component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/test"!</div>
+	async function fetchRustObject(): Promise<TestObj> {
+		try {
+			const response = await axios.get<TestObj>('/v1/test');
+			return response.data;
+		} catch (error) {
+			console.error(error);
+			if (axios.isAxiosError(error)) {
+				throw new Error(error.response?.data?.message || error.message || 'Failed to acquire test object');
+			} else {
+				throw new Error("Unexpected error");
+			}
+		}
+
+	}
+	const info = useQuery({ queryKey: ['hello'], queryFn: fetchRustObject })
+
+	if (info.status === 'pending') {
+		return <span> Loading... </span>
+	}
+	if (info.status === 'error') {
+		return <span>Error: {info.error.message} </span>
+	}
+
+	return (
+		<span>
+			{info.data.name}
+		</span>
+	)
+
 }

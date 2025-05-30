e99e1fae7335ae9e0b80619e02ae1264886edaec
Daniel Jin <mrpickles123115@gmail.com>
2025-05-29 21:02:09 -0600

    add game state

--- backend/src/state.rs
+++ backend/src/state.rs
@@ -5,7 +5,77 @@ use std::{
 
 use tokio::sync::mpsc;
 
+pub type RoomMap = Arc<Mutex<HashMap<u64, GameRoom>>>;
+pub type PlayerId = String;
+
 #[derive(Clone)]
 pub struct AppState {
-    pub rooms: Arc<Mutex<HashMap<u64, mpsc::UnboundedSender<String>>>>,
+    pub rooms: RoomMap,
+}
+
+pub struct GameRoom {
+    pub room_number: u64,
+    pub tx: mpsc::Sender<PlayerAction>,
+}
+
+pub enum PlayerRole {
+    Spectating,
+    Playing(u64), //Current player order
+}
+
+pub struct PlayerInformation {
+    role: PlayerRole,
+    nickname: String,
+    player_id: PlayerId,
+    last_heartbeat: u64,
+}
+
+pub struct GameState {
+    connections: HashMap<PlayerId, PlayerInformation>,
+}
+
+pub enum PlayerAction {
+    Join(PlayerInformation),
+    Heartbeat,
+}
+
+pub enum GameAction {
+    SetCards,
+    Layout,
+    CurrentPlayers,
+    Private,
+    Heartbeat,
+}
+
+/* pub struct ActiveGame {
+    pub connections : Vec<
+} */
+
+impl GameRoom {
+    pub async fn start_room(rooms: RoomMap) -> Result<u64, String> {
+        let room_id = random_number();
+        let mut room_map = rooms.lock().unwrap();
+
+        if room_map.contains_key(&room_id) {
+            return Result::Err(String::from("Failed to get available room"));
+        }
+        let (tx, mut rx) = mpsc::channel::<PlayerAction>(32);
+        let new_room = GameRoom {
+            room_number: room_id,
+            tx,
+        };
+
+        room_map.insert(room_id, new_room);
+        Result::Ok(room_id)
+    }
+
+    pub async fn game_loop(mut rx: mpsc::Receiver<PlayerAction>) {
+        while let Some(msg) = rx.recv().await {
+            //Do stuff here
+        }
+    }
+}
+
+fn random_number() -> u64 {
+    4
 }
--- backend/src/wss/rooms.rs
+++ backend/src/wss/rooms.rs
@@ -1,20 +1,55 @@
-use tokio::sync::mpsc;
-use tokio_tungstenite::client_async;
-
 use std::{
     collections::HashMap,
     net::SocketAddr,
     sync::{Arc, Mutex},
 };
 
-use serde::{Deserialize, Serialize};
-use ts_rs::TS;
+use axum::{
+    Json,
+    extract::{State, WebSocketUpgrade, ws::WebSocket},
+    http::StatusCode,
+    response::IntoResponse,
+};
 
-use tracing::{info, instrument};
+use tokio::sync::mpsc;
 
-/* type TX =
-struct PlayerInfo {
-    tx : TX;
+use crate::state;
+
+#[tokio::main]
+
+async fn create(State(state): State<state::AppState>) -> impl IntoResponse {
+    let rooms = state.rooms.clone();
+    let (tx, mut rx) = mpsc::channel::<String>(32);
+
+    let room_id = random_number();
+    {
+        let mut room_map = rooms.lock().unwrap();
+
+        //erm what the skibidi how did my random number generator fail???
+        if room_map.contains_key(&room_id) {
+            return Result::Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    tokio::spawn(async move { game_task(room_id, rooms) });
+
+    Result::Ok(Json(room_id))
+
+    //TODO: Check for existance
 }
 
-type RoomMap = Arc<Mutex<HashMap<SocketAddr, PlayerInfo>>>; */
+async fn game_task(room_number: u64, rooms: state::RoomMap) {}
+
+async fn join(
+    room: u64,
+    ws: WebSocketUpgrade,
+    State(state): State<state::AppState>,
+) -> impl IntoResponse {
+    String::from("todo")
+}
+
+//async fn handle_create_websocket(
+
+fn random_number() -> u64 {
+    4
+}

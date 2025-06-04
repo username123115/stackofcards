5fb6e665d784131c8eab4a2fe19955b9d09f313d
Daniel Jin <mrpickles123115@gmail.com>
2025-06-04 23:20:15 +0800

    comment on state submodule purposes

--- backend/src/state/mod.rs
+++ backend/src/state/mod.rs
@@ -1,4 +1,4 @@
-pub mod app;
-pub mod game;
+pub mod app; //Manipulation of global state
+pub mod game; //Wraps engine state + apis (this is TODO, not actually wrapping yet)
 pub mod player;
-pub mod web;
+pub mod web; //Thread listening and responding to websocket clients //Tracking active players in a web game

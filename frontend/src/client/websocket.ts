0d81c08a82cc9346bf0a603d3efe089452bbb296
Daniel Jin <mrpickles123115@gmail.com>
2025-06-05 18:02:56 +0800

    game lobby makes connection

---
+++ frontend/src/client/websocket.ts
@@ -0,0 +1,43 @@
+import type { GameSnapshot } from '@bindings/GameSnapshot'
+
+export function connectToGame(
+	roomId: number,
+	onSnapshot: (snapshot: GameSnapshot) => void,
+	onErrorCallback: (error: Event) => void,
+	onCloseCallback: () => void
+): WebSocket {
+	// TODO: Have server pass back an upgrade url when GETing /v1/rooms/{roomId}
+	const wsUrl = `ws://localhost:5050/v1/rooms/${roomId}`;
+	console.log(`Connectiong to ${wsUrl}`);
+
+	let socket = new WebSocket(wsUrl);
+	socket.onopen = () => {
+		console.log(`Successfully connected to WebSocket room ${roomId}`);
+		// Backend will handle a Join/Rejoin, now the client should make sure to receive a private message containing their ID
+	}
+
+	socket.onmessage = (event) => {
+		try {
+			const rawData = JSON.parse(event.data as string);
+			const snapshot = rawData as GameSnapshot;
+
+			snapshot ? onSnapshot(snapshot) : console.warn("Received invalid Game Snapshot");
+		}
+		catch (e) {
+			console.error(`Error parsing message from server: ${e} \n Raw: ${event.data}`);
+		}
+	}
+
+	socket.onerror = (error) => {
+		console.error(`WebSocket error: ${error}`);
+		onErrorCallback(error);
+	}
+
+	socket.onclose = () => {
+		console.log(`WebSocket connection to room ${roomId} has closed`);
+		onCloseCallback();
+	}
+	return socket;
+
+
+}
--- frontend/src/routes/games/$gameId.tsx
+++ frontend/src/routes/games/$gameId.tsx
@@ -1,15 +1,31 @@
 import { createFileRoute, useParams } from '@tanstack/react-router'
+import { connectToGame } from '@client/websocket'
+
+import type { GameSnapshot } from '@bindings/GameSnapshot'
 
 export const Route = createFileRoute('/games/$gameId')({
 	component: RouteComponent,
 })
 
 function RouteComponent() {
+
+	function onSnapshot(snapshot: GameSnapshot) {
+		console.info(snapshot);
+	}
+
+	function onErrorCallback(error: Event) {
+	}
+
+	function onCloseCallback() {
+	}
+
 	const { gameId } = Route.useParams();
 	const code = Number(gameId);
+
+	const socket = connectToGame(code, onSnapshot, onErrorCallback, onCloseCallback);
 
 	//TODO: This should be passed as some parameter
 	const username: String = "ninebitcomputer";
 
-	return <div>Hello "/games/$gameid"!</div>
+	return <div>{`Playing game ${code}`}</div>
 }

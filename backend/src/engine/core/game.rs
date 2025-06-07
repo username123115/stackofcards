9c882b1983ffdea0ce3c3e3a4767e69287088651
Daniel Jin <mrpickles123115@gmail.com>
2025-06-07 15:18:46 +0800

    add some ways to choose players and zones

--- backend/src/engine/core/game.rs
+++ backend/src/engine/core/game.rs
@@ -3,26 +3,51 @@ use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 use ts_rs::TS;
 
-pub type OrderIdentifier = String;
-pub type PatternIdentifier = String;
-pub type VariableIdentifier = String;
+pub type Identifier = String;
+
+pub type VariableIdentifier = Identifier;
+pub type ZoneClassIdentifier = Identifier;
+pub type PlayerClassIdentifier = Identifier;
 
 pub enum ZoneVisibility {
     Owner,
     All,
 }
 
-pub struct ZoneDef {
+pub struct ZoneClass {
     visibility: ZoneVisibility,
-    rules: Vec<PatternIdentifier>, //Cards here after to match one of these patterns
+    rules: Vec<patterns::PatternIdentifier>, //Cards here after to match one of these patterns
+}
+
+pub struct PlayerClass {
+    zones: HashMap<VariableIdentifier, ZoneClassIdentifier>,
+}
+
+pub enum ZoneTarget {
+    Existing(VariableIdentifier), // one of the initial zones
+    Class(ZoneClassIdentifier),   // any of zone type
+    Player {
+        // player + desired zone for given player type
+        player: PlayerChoice,
+        zone: HashMap<PlayerClassIdentifier, VariableIdentifier>,
+    },
+}
+
+pub enum PlayerChoice {
+    Increment(u64),                               //nth player after this one by order
+    IncrementByClass(PlayerClassIdentifier, u64), //nth player after this one of this class
+    All,                                          //all players
+    AllByClass(PlayerClassIdentifier),            //all players of this class
 }
 
 pub struct GameConfig {
     pub allowed_ranks: HashSet<cards::Rank>,
     pub allowed_suits: HashSet<cards::Suit>,
-    pub orders: HashMap<OrderIdentifier, cards::RankOrder>,
-    pub patterns: HashMap<PatternIdentifier, patterns::Pattern>,
-    pub initial_zones: HashMap<VariableIdentifier, ZoneDef>, //Engine sets these up first
+    pub orders: HashMap<cards::OrderIdentifier, cards::RankOrder>,
+    pub patterns: HashMap<patterns::PatternIdentifier, patterns::Pattern>,
+    pub zone_classes: HashMap<ZoneClassIdentifier, ZoneClass>,
+
+    pub initial_zones: HashMap<VariableIdentifier, ZoneClassIdentifier>, //Engine sets these up first and
 }
 
 // This describes ranks.len * suits.len cards

1e132d3d86d5e9447468d8379acde66ec9079ca1
Daniel Jin <mrpickles123115@gmail.com>
2025-06-22 13:09:50 +0800

    add axum login

--- backend/src/state/mod.rs
+++ backend/src/state/mod.rs
@@ -1,9 +1,4 @@
 pub mod app; //Manipulation of global state
+pub mod auth;
 pub mod engine_wrapper;
 pub mod web;
-/*
-pub mod game_wrapper; //Wraps engine state + apis (this is TODO, not actually wrapping yet)
-pub mod names;
-pub mod player;
-pub mod web; //Thread listening and responding to websocket clients //Tracking active players in a web game
-*/

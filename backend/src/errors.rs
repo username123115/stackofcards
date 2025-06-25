77bbb4307089ef4c9db1c1c922c1fca8d73e4542
Daniel Jin <mrpickles123115@gmail.com>
2025-06-25 08:40:11 +0800

    remove logging again

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -1,9 +1,5 @@
-use crate::state::auth;
 use axum::{Json, http::StatusCode};
 use serde::{Deserialize, Serialize};
-
-use crate::state::app::AppState;
-use axum_extra::extract::cookie::CookieJar;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ErrorDetail {
@@ -19,11 +15,4 @@ pub fn new_web_error(status: StatusCode, message: &str) -> WebError {
             error: message.to_string(),
         }),
     )
-}
-
-pub async fn auth_or_error(state: AppState, jar: CookieJar) -> Result<auth::UserSession, WebError> {
-    match auth::user_auth(state, jar).await {
-        Ok(s) => Ok(s),
-        Err(_) => Err(new_web_error(StatusCode::FORBIDDEN, "not logged in")),
-    }
 }

384627ac345ab4a108de5586f42235881db05b9b
Daniel Jin <mrpickles123115@gmail.com>
2025-06-28 14:08:04 +0800

    add var mapper

--- frontend/src/components/editor/config/playerTemplate.tsx
+++ frontend/src/components/editor/config/playerTemplate.tsx
@@ -1,77 +1,52 @@
 import type { GameConfig } from '@client/types/engine/config'
 
-import { renameProperty, NameFieldComponent } from './utility'
+import { ConfigItemList, } from './utility'
+import type { ModifiableConfigProps, ConfigMapping } from './utility'
 
 import styles from './config.module.css'
 
 export default function VariableMappingList({ config, variableMapping, handleEditMapping = null }:
-	{ config: GameConfig, variableMapping: GameConfig['player_zones'], handleEditMapping: ((template: GameConfig['player_zones']) => void) | null }) {
+	{ config: GameConfig, variableMapping: { [key in string]: string | undefined }, handleEditMapping: ((contents: ConfigMapping<string>) => void) | null }) {
 
-	function RenameVar(newName: string, oldName: string) {
-		if (handleEditMapping) {
-			const result = renameProperty(variableMapping, newName, oldName);
-			if (result) {
-				handleEditMapping(result);
-			}
-		}
-	}
-	function AddNewVar() {
-		if (handleEditMapping) {
-			let untitledVars = 0;
-			while (variableMapping[`new_var_${untitledVars}`]) {
-				untitledVars += 1;
-			}
-			const newVarName = `new_var_${untitledVars}`;
 
-			const updated = {
-				...variableMapping,
-				[newVarName]: "",
-			}
-			handleEditMapping(updated);
-		}
-	}
-
-	const template = Object.entries(variableMapping).map(
-		([varName, classRef]) => {
-			return (
-				<li key={varName}>
-					<div className={styles.horizontalList}>
-						<NameFieldComponent name={varName} editName={handleEditMapping ? (newName) => { RenameVar(newName, varName) } : null} />
-						<div className={styles.rounded}>
-							{
-								handleEditMapping ?
-									<div>
-										<select value={classRef} onChange={(e) => {
-											if (e.target.value && e.target.value !== "") {
-												handleEditMapping({ ...variableMapping, [varName]: e.target.value })
-											}
-										}}>
-											<option value=""> </option>
-											{
-												Object.keys(config.zone_classes).map(
-													(z) => <option value={z}> {z} </option>
-												)
-											}
-										</select>
-
-									</div>
-									:
-									<div> {classRef} </div>
-							}
+	function TypeNameComponent(props: ModifiableConfigProps<string>) {
+		return (
+			<div className={styles.rounded}>
+				{
+					props.setConfigItem ?
+						<div>
+							<select value={props.configItem} onChange={(e) => {
+								if (e.target.value && e.target.value !== "") {
+									props.setConfigItem!(e.target.value)
+								}
+							}}>
+								<option value=""> </option>
+								{
+									Object.keys(config.zone_classes).map(
+										(z) => <option value={z}> {z} </option>
+									)
+								}
+							</select>
 						</div>
-					</div>
-				</li >
-			)
-		}
-	)
+						:
+						<div>
+							{props.configItem}
+						</div>
+				}
+			</div>
+		)
+	}
 
-	return (
-		<div>
-			<ul className={styles.elementListing}>
-				{template}
-				{handleEditMapping && <button className={styles.menuButton} onClick={AddNewVar}> Add Variable </button>}
-			</ul>
-		</div>)
+
+	return <ConfigItemList
+		Component={TypeNameComponent}
+		config={config}
+		contents={variableMapping}
+		defaultItem={() => ""}
+		updateContents={handleEditMapping}
+		prefix="new_var"
+		displayInline={true}
+	/>
 
 }
 
